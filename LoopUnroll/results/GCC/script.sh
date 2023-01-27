782b02495b36b13e1d014d0da6ba450eb4cb7b5c
RIM <AchRiim2001@gmail.com>
2023-01-27 05:10:50 +0100

    Histogrammes de perf avec GCC pour un code déroulé

---
+++ LoopUnroll/results/GCC/script.sh
@@ -0,0 +1,29 @@
+set terminal pngcairo font "Arial,12" fontscale 1.8 size 1400, 700
+set output 'Unroll_gcc.jpg'
+
+set boxwidth 1.0
+set style fill  solid 1.0 
+set key fixed top right
+set key box 
+set style histogram clustered gap 3  title textcolor lt -1 
+set style data histograms
+set grid 
+#set offset -1,0,0,0
+
+
+set xrange [ 0 : * ] 
+set xlabel " gcc optimisation levels"
+set yrange [ * : * ] 
+set ylabel " ns"
+
+
+
+
+set title 'performances for different levels of optimisation using gcc compiler(length=2800)'
+
+#tracer les histogrammes en utilisant les données de la deuxième colonne des fichiers.dat
+
+plot 'unroll_gcc_-Ofast.dat' using 2:xtic(2) ti "-Ofast" lc rgb '#FFFF00','unroll_gcc_-O0.dat' using 2:xtic(2) ti "-O0" lc rgb  '#F7DC6F', 'unroll_gcc_-O1.dat' using 2:xtic(2) ti "-O1" lc rgb '#F4D03F', 'unroll_gcc_-O2.dat' using 2:xtic(2) ti "-O2" lc rgb '#D4AC0D ', 'unroll_gcc_-O3.dat' using 2:xtic(2) ti "-O3" lc rgb '#9A7D0A' , 'unroll_gcc_-Og.dat' using 2:xtic(2) ti "-Og" lc rgb '#FFA500 '
+
+replot
+
